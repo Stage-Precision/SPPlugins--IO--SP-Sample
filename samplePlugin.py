d0bd4ce0fe0de0b49690d894bd14c15de053f794
Michael Hantelmann <mh@stageprecision.com>
2023-09-25 19:56:52 +0200

    update structure of py sp

--- samplePlugin.py
+++ samplePlugin.py
@@ -32,16 +32,29 @@ class SampleModule(sp.BaseModule):
 		subContainer = self.addContainer("Setting")
 		self.stringpara = subContainer.addStringParameter("Eample", "sring")
 
-		#create actions with definable parameters 
-		action = self.addAction("Run Example Action", "sendExample", self.onRunExampleAction)
+		#create actions with definable parameters
+		# (nicename, folder, function)
+		action = self.addAction("Run Example Action", "", self.onRunExampleAction)
 		action.addStringParameter("Message", "mes")
 
 		#create event to react inside SP events
 		self.registerEvent("Example Event", "exampleEvent")
+
+		#create a timer callback to run in cycle - in this example every 30s 
+		self.addTimer("checker", 30, self.checkfunction)
 		
+	# function to react on user change of parameters - it will call by change by user or remoted
+	def onParameterFeedback(self, parameter):
+		if parameter == self.stringPara:
+			print("string parameter changed"+ self.stringPara.value)
+
 	def onRunExampleAction(self, sMessage):
 		print(sMessage+ self.stringPara.value)
 		self.emitEvent("exampleEvent")
+
+	def checkfunction(self):
+		print("timer called")
+
 
 if __name__ == "__main__":
     sp.registerPlugin(SampleModule)
